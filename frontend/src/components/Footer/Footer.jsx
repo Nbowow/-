3b2c85138f38fd19a84e506d009ef98a5e738bab
unknown <ks00919@naver.com>
2024-09-12 16:02:15 +0900

    feat: Footer 생성

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import * as S from "./Footer.styled";
+
+const Footer = () => {
+    return (
+        <S.Section>
+            <S.FooterLogo src="./logo/logo_dark_green.svg" />
+            <div>SSAFY 11기 특화 프로젝트 빅데이터 분산 C206</div>
+            <div>김민주 이지영 김정민 박기창 남보우 신재건</div>
+        </S.Section>
+    );
+};
+
+export default Footer;
---
+++ frontend/src/components/Footer/Footer.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+    height: fit-content;
+    padding: 27;
+    background-color: #606060;
+`;
+
+export const FooterLogo = styled.img`
+    height: 5%;
+`;
