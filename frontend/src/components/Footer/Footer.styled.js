8e1502229c4a93ad0e85103242188a4fe016b0eb
unknown <ks00919@naver.com>
2024-09-12 17:09:17 +0900

    style: footer 스타일 변경

--- frontend/src/components/Footer/Footer.styled.js
+++ frontend/src/components/Footer/Footer.styled.js
@@ -1,11 +1,26 @@
 import styled from "styled-components";
 
 export const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    gap: 20px;
+    padding: 50px;
     height: fit-content;
-    padding: 27;
-    background-color: #606060;
+    background-color: ${({ theme }) => theme.color.gray.darker};
 `;
 
 export const FooterLogo = styled.img`
-    height: 5%;
+    width: 101px;
+`;
+
+export const Info = styled.div`
+    font-family: "SUITRegular";
+    color: ${({ theme }) => theme.color.gray.light};
+    font-size: ${({ theme }) => theme.fontSize.text};
+    padding: 0.17rem;
+    line-height: 1.7;
+    text-align: center;
 `;
