9ce5ab62ce393e7d38eac9aae09c44cceb4795d0
unknown <ks00919@naver.com>
2024-09-13 14:13:52 +0900

    fix: notion 아이콘 잘못된 경로 수정

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
                 <div>SSAFY 11기 특화 프로젝트 빅데이터 분산 C206</div>
                 <div>김민주 이지영 김정민 박기창 남보우 신재건</div>
             </S.Info>
-            <img src="./icon/notion-logo.svg" />
+            <img src="./logo/notion-logo.svg" />
         </S.Section>
     );
 };
