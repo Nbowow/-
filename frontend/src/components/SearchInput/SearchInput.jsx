3168266ed3df8d806a41a5596d602d51236161e4
Minju <69795199+ks00919@users.noreply.github.com>
2024-09-17 22:57:29 +0900

    design: #S11P21C206-226 검색창 기본 css 설정

--- frontend/src/components/SearchInput/SearchInput.jsx
+++ frontend/src/components/SearchInput/SearchInput.jsx
@@ -1,18 +1,17 @@
 import * as S from "./SearchInput.styled";
-import propTypes from "prop-types";
 
-const SearchInput = ({ text }) => {
+const SearchInput = () => {
     return (
-        <S.SearchContainer placeholder={text}>
-            <div>🔍</div>
-            <span>오늘은 무슨 요리를 할까요?</span>
-            <span>재료와 요리명으로 추천 레시피를 검색해보세요.</span>
+        <S.SearchContainer>
+            <S.Icon>🔍</S.Icon>
+            <S.Text>
+                <span className="bold">오늘은 무슨 요리를 할까요?</span>
+                <span className="gray">
+                    재료와 요리명으로 추천 레시피를 검색해보세요.
+                </span>
+            </S.Text>
         </S.SearchContainer>
     );
-};
-
-SearchInput.propTypes = {
-    text: propTypes.string.isRequired,
 };
 
 export default SearchInput;
--- frontend/src/components/SearchInput/SearchInput.styled.js
+++ frontend/src/components/SearchInput/SearchInput.styled.js
@@ -2,16 +2,40 @@ import styled from "styled-components";
 
 export const SearchContainer = styled.div`
     position: relative;
-    width: 1250px;
-    height: 110px;
+    width: 78.125rem;
+    height: 6.875rem;
+    gap: 1.5rem;
 
-    border: 1px solid ${({ theme }) => theme.color.gray.lighter};
-    border-radius: 100px;
+    border: 0.063rem solid ${({ theme }) => theme.color.gray.lighter};
+    border-radius: 6.25rem;
 
-    box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.2);
+    box-shadow: 0px 0.25rem 1.25rem rgba(0, 0, 0, 0.2);
 
-    padding-left: 70px;
+    padding-left: 4.375rem;
 
     display: flex;
     align-items: center;
+`;
+
+export const Icon = styled.div`
+    font-family: "TossFace";
+    font-size: 1.875rem;
+`;
+
+export const Text = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.313rem;
+
+    .bold {
+        font-family: "SUITSemiBold";
+        font-size: ${({ theme }) => theme.fontSize.h3};
+    }
+
+    .gray {
+        font-family: "SUITMedium";
+        font-size: ${({ theme }) => theme.fontSize.h4};
+        color: ${({ theme }) => theme.color.gray.light};
+    }
 `;
