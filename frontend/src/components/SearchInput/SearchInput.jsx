8d9536a1e60e4774f4edb2c29cc4fd5ddc4569e3
unknown <ks00919@naver.com>
2024-09-19 16:12:08 +0900

    feat: #S11P21C206-226 검색창 컴포넌트 기능 개발

--- frontend/src/components/SearchInput/SearchInput.jsx
+++ frontend/src/components/SearchInput/SearchInput.jsx
@@ -1,17 +1,123 @@
 import * as S from "./SearchInput.styled";
+import { useState, useRef, useEffect, useCallback } from "react";
+import PropTypes from "prop-types";
+import SearchHistory from "./SearchHistory";
+import useSearchHistoryStore from "../../store/searchHistoryStore";
 
-const SearchInput = () => {
-    return (
-        <S.SearchContainer>
-            <S.Icon>🔍</S.Icon>
-            <S.Text>
-                <span className="bold">오늘은 무슨 요리를 할까요?</span>
-                <span className="gray">
-                    재료와 요리명으로 추천 레시피를 검색해보세요.
-                </span>
-            </S.Text>
-        </S.SearchContainer>
+const SearchInput = ({
+    userId,
+    purpose,
+    boldPlacehold,
+    grayPlaceHold,
+    onSubmit,
+}) => {
+    const [isFocused, setIsFocused] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+    const { getSearchTerm, addSearchTerm, removeSearchTerm } =
+        useSearchHistoryStore();
+
+    const searchInputRef = useRef(null);
+    const inputRef = useRef(null);
+
+    const searchHistory =
+        userId && purpose ? getSearchTerm(userId, purpose) : [];
+
+    useEffect(() => {
+        const handleOutsideClose = (e) => {
+            if (
+                searchInputRef.current &&
+                !searchInputRef.current.contains(e.target)
+            )
+                setIsFocused(false);
+        };
+        document.addEventListener("mousedown", handleOutsideClose);
+
+        return () =>
+            document.removeEventListener("mousedown", handleOutsideClose);
+    }, [isFocused]);
+
+    const handleInputFocus = useCallback(() => {
+        setIsFocused(true);
+    }, []);
+
+    const handleSearchInputChange = useCallback((e) => {
+        setSearchValue(e.target.value);
+    }, []);
+
+    const handleSearchhistoryClick = useCallback(
+        (term) => {
+            setSearchValue(term);
+            onSubmit(term);
+        },
+        [onSubmit],
     );
+
+    const handleSearchSubmit = useCallback(
+        (e) => {
+            if (e.key === "Enter" && searchValue.trim()) {
+                if (userId) addSearchTerm(userId, purpose, searchValue);
+                setIsFocused(false);
+                onSubmit(searchValue);
+            }
+        },
+        [searchValue, userId, purpose, addSearchTerm, onSubmit],
+    );
+
+    return (
+        <S.SearchInput ref={searchInputRef}>
+            {isFocused ? (
+                <S.SearchFocusContainer>
+                    <S.SearchInputForm>
+                        <div className="input">
+                            <S.Icon>🔍</S.Icon>
+                            <input
+                                autoFocus
+                                value={searchValue}
+                                ref={inputRef}
+                                onChange={handleSearchInputChange}
+                                onKeyDown={handleSearchSubmit}
+                            />
+                        </div>
+                        <S.Line />
+                        {searchHistory.map((term, index) => (
+                            <SearchHistory
+                                key={index}
+                                term={term}
+                                onClick={() => {
+                                    handleSearchhistoryClick(term);
+                                }}
+                                onButtonClick={() =>
+                                    removeSearchTerm(userId, purpose, index)
+                                }
+                            />
+                        ))}
+                    </S.SearchInputForm>
+                </S.SearchFocusContainer>
+            ) : (
+                <S.SearchContainer onClick={handleInputFocus}>
+                    <S.Icon>🔍</S.Icon>
+                    <S.Text>
+                        {searchValue ? (
+                            <span className="plain">{searchValue}</span>
+                        ) : (
+                            <>
+                                <span className="bold">{boldPlacehold}</span>
+                                <span className="gray">{grayPlaceHold}</span>
+                            </>
+                        )}
+                    </S.Text>
+                </S.SearchContainer>
+            )}
+        </S.SearchInput>
+    );
+};
+
+SearchInput.propTypes = {
+    userId: PropTypes.string.isRequired,
+    purpose: PropTypes.string.isRequired,
+    boldPlacehold: PropTypes.string,
+    grayPlaceHold: PropTypes.string,
+    onSubmit: PropTypes.func.isRequired,
 };
 
 export default SearchInput;
