531e4443c1fb5b60ebb537c83fa0b2c7bbfd37d0
unknown <ks00919@naver.com>
2024-09-20 09:34:29 +0900

    feat: #S11P21C206-242 버튼 컴포넌트 개발

--- frontend/src/components/Button/SubmitButton/SubmitButton.jsx
+++ frontend/src/components/Button/SubmitButton/SubmitButton.jsx
@@ -1,7 +1,21 @@
 import * as S from "./SubmitButton.styled";
+import PropTypes from "prop-types";
 
-const SubmitButton = () => {
-    return <S.SubmitButton>정보 수정</S.SubmitButton>;
+const SubmitButton = ({ text, onClick, width, height }) => {
+    return (
+        <>
+            <S.SubmitButton onClick={onClick} width={width} height={height}>
+                {text}
+            </S.SubmitButton>
+        </>
+    );
+};
+
+SubmitButton.propTypes = {
+    text: PropTypes.string,
+    onClick: PropTypes.func,
+    width: PropTypes.string,
+    height: PropTypes.string,
 };
 
 export default SubmitButton;
