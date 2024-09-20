90cc1a02fdd59995b6f5402165ae12e1b507de67
unknown <ks00919@naver.com>
2024-09-20 09:44:07 +0900

    docs: #S11P21C206-242 SubmitButton JSDoc 작성

--- frontend/src/components/Button/SubmitButton/SubmitButton.jsx
+++ frontend/src/components/Button/SubmitButton/SubmitButton.jsx
@@ -4,10 +4,22 @@ import PropTypes from "prop-types";
 /**
  * SubmitButton 컴포넌트
  *
- * 너비와 높이를 props로 받아서 쓰는 컴포넌트, 입력하지 않으면 100px, 40px 고정
+ * 텍스트와 클릭 핸들러를 받아 버튼을 렌더링합니다.
+ * 또한 너비(`width`)와 높이(`height`)를 props로 받아 버튼의 크기를 설정하며,
+ * props가 전달되지 않을 경우 기본적으로 `100px`의 너비와 `40px`의 높이를 가집니다.
  *
- * @param {*} param0
- * @returns
+ * @component
+ * @example
+ * return (
+ *   <SubmitButton text="제출하기" onClick={handleSubmit} width="150px" height="50px" />
+ * )
+ *
+ * @param {Object} props - SubmitButton의 props
+ * @param {string} props.text - 버튼에 표시될 텍스트
+ * @param {function} props.onClick - 버튼 클릭 시 호출될 함수
+ * @param {string} [props.width="100px"] - 버튼의 너비 (optional)
+ * @param {string} [props.height="40px"] - 버튼의 높이 (optional)
+ * @returns {JSX.Element} SubmitButton 컴포넌트
  */
 const SubmitButton = ({ text, onClick, width, height }) => {
     return (
