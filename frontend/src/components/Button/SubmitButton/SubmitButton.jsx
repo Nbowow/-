bd227459526093409782158e7f5b33187319dea2
unknown <ks00919@naver.com>
2024-09-20 09:40:44 +0900

    docs: #S11P21C206-242 SubmitButton JSDoc 작성

--- frontend/src/components/Button/SubmitButton/SubmitButton.jsx
+++ frontend/src/components/Button/SubmitButton/SubmitButton.jsx
@@ -1,6 +1,14 @@
 import * as S from "./SubmitButton.styled";
 import PropTypes from "prop-types";
 
+/**
+ * SubmitButton 컴포넌트
+ *
+ * 너비와 높이를 props로 받아서 쓰는 컴포넌트, 입력하지 않으면 100px, 40px 고정
+ *
+ * @param {*} param0
+ * @returns
+ */
 const SubmitButton = ({ text, onClick, width, height }) => {
     return (
         <>
