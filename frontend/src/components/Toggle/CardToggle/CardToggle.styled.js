3becec3405947fe6d9dfe3c76bcb691f347cad54
unknown <ks00919@naver.com>
2024-09-20 12:12:13 +0900

    design: #S11P21C206-242 카드 토글 CSS

---
+++ frontend/src/components/Toggle/CardToggle/CardToggle.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const CardToggle = styled.div`
+    width: 1.563rem;
+    height: 1.563rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    cursor: pointer;
+`;
