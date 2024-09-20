14589ce620beb8d2a21a68817c90ee4bb04d3e36
정민 <jmeve24@naver.com>
2024-09-20 14:49:30 +0900

    refactor: #S11P21C206-173  styled-component 코드와 분리

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import styled from "styled-components";
 import PropTypes from "prop-types";
-
+import * as S from "./Modal.styled";
 function Modal({ context }) {
     const [isOpen, setIsOpen] = useState(true);
 
@@ -12,54 +11,17 @@ function Modal({ context }) {
     if (!isOpen) return null;
 
     return (
-        <MyModal>
-            <MyPageModal>
-                <CloseButton onClick={handleClose}>닫기</CloseButton>
+        <S.ModalContainer>
+            <S.Modal>
+                <S.CloseButton onClick={handleClose}>닫기</S.CloseButton>
                 {context}
-            </MyPageModal>
-        </MyModal>
+            </S.Modal>
+        </S.ModalContainer>
     );
 }
 
 Modal.propTypes = {
-    context: PropTypes.node.isRequired, // context는 컴포넌트나 JSX로 렌더링 가능한 모든 것
+    context: PropTypes.node.isRequired,
 };
-export const MyModal = styled.div`
-    background-color: rgba(0, 0, 0, 0.4);
-    width: 100%;
-    height: 100vh;
-    z-index: 100;
-    position: fixed;
-    top: 0;
-    left: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
-
-export const MyPageModal = styled.div`
-    width: 40em;
-    height: 30rem;
-    background-color: white;
-    position: relative;
-    border-radius: 10px;
-    box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
-    z-index: 101;
-    padding: 20px;
-`;
-
-export const CloseButton = styled.button`
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    background-color: transparent;
-    border: none;
-    font-size: 1rem;
-    cursor: pointer;
-    color: ${({ theme }) => theme.color.gray.dark};
-    &:hover {
-        color: ${({ theme }) => theme.color.point.green};
-    }
-`;
 
 export default Modal;
