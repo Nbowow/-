40d053f9eef7c4f54c79ea05517e3666e2409d3a
Minju <69795199+ks00919@users.noreply.github.com>
2024-09-22 12:51:22 +0900

    refactor: #S11P21C206-243 defaultProps 삭제

--- frontend/src/components/Card/IntroCard/IntroCard.jsx
+++ frontend/src/components/Card/IntroCard/IntroCard.jsx
@@ -18,7 +18,7 @@ import PropTypes from "prop-types";
  * const imgUrl = "/images/sample.jpg";
  * return <IntroCard title={title} text={text} imgUrl={imgUrl} />;
  */
-const IntroCard = ({ imgUrl, title, text }) => {
+const IntroCard = ({ imgUrl = "", title, text }) => {
     const placeholderImage = "/images/placeholder-img.jpg";
 
     return (
@@ -41,10 +41,6 @@ IntroCard.propTypes = {
     imgUrl: PropTypes.string,
     title: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
-};
-
-IntroCard.defaultProps = {
-    imgUrl: "",
 };
 
 export default IntroCard;
