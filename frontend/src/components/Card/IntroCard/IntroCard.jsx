2f584825e05ed0f1973608689f6bca19c6330b9b
Minju <69795199+ks00919@users.noreply.github.com>
2024-09-22 11:55:19 +0900

    feat: #S11P21C206-243 메인페이지 게시글 소개 카드 개발

--- frontend/src/components/Card/IntroCard/IntroCard.jsx
+++ frontend/src/components/Card/IntroCard/IntroCard.jsx
@@ -1,15 +1,28 @@
 import * as S from "./IntroCard.styled";
+import PropTypes from "prop-types";
 
-// TODO: 테스트용 이미지 삭제
-const testImageURL =
-    "https://d2u3dcdbebyaiu.cloudfront.net/uploads/atch_img/841/5f8e4c9d182e64548f58cc628427754b_res.jpeg";
+const IntroCard = ({ imgUrl, title, text }) => {
+    const placeholderImage = "/images/placeholder-img.jpg";
 
-const IntroCard = () => {
+    const handleError = (e) => {
+        e.target.src = placeholderImage;
+    };
+
     return (
         <S.IntroCard>
-            <S.Thumnail src={testImageURL}></S.Thumnail>
+            <S.Thumnail src={imgUrl} onError={handleError} />
+            <S.TextArea>
+                <S.Title>{title}</S.Title>
+                <S.Text>{text}</S.Text>
+            </S.TextArea>
         </S.IntroCard>
     );
+};
+
+IntroCard.propTypes = {
+    imgUrl: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
 };
 
 export default IntroCard;
