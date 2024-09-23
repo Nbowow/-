1fa593666c214a9183f5b6890cdc40b8d880f469
unknown <ks00919@naver.com>
2024-09-23 16:55:59 +0900

    feat: #S11P21C206-262 카드 토글 개발

---
+++ frontend/src/components/Toggle/CardToggle/CardToggle.jsx
@@ -0,0 +1,24 @@
+import * as S from "./CardToggle.styled";
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+const CardToggle = ({ imgUrl, text }) => {
+    const [isActive, setIsActive] = useState(false);
+
+    return (
+        <S.CardToggle
+            isActive={isActive}
+            onClick={() => setIsActive(!isActive)}
+        >
+            <S.Image src={imgUrl}></S.Image>
+            <S.Text>{text}</S.Text>
+        </S.CardToggle>
+    );
+};
+
+CardToggle.propTypes = {
+    imgUrl: PropTypes.string,
+    text: PropTypes.string,
+};
+
+export default CardToggle;
