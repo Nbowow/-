e6274b838c5bda3ded5125f2885bf433ae81a858
unknown <ks00919@naver.com>
2024-09-23 11:59:38 +0900

    refactor: #S11P21C206-243 카드 CSS 공통 속성 리팩토링

--- frontend/src/components/Card/IntroCard/IntroCard.styled.js
+++ frontend/src/components/Card/IntroCard/IntroCard.styled.js
@@ -1,73 +1,52 @@
 import styled from "styled-components";
+import {
+    cardBorderRadius,
+    BaseCard,
+    BaseThumnail,
+    BaseTitle,
+    BaseText,
+    BaseTextArea,
+} from "../Card.styled";
 
-const cardBorderRadius = "1.25rem";
-
-export const IntroCard = styled.div`
+export const IntroCard = styled(BaseCard)`
     width: 17.25rem;
     height: 22.438rem;
     border-radius: ${cardBorderRadius};
-
-    box-sizing: border-box;
     box-shadow: 0px 0.25rem 1.25rem rgba(0, 0, 0, 0.2);
-
-    display: flex;
-    flex-direction: column;
-    gap: 1.125rem;
-
-    padding-bottom: 1.25rem;
     cursor: pointer;
-
     transition:
         transform 0.6s cubic-bezier(0.25, 0.8, 0.25, 1),
         box-shadow 0.6s cubic-bezier(0.25, 0.8, 0.25, 1);
-
-    &:active {
-        background-color: ${({ theme }) => theme.color.point.lightGreen};
-    }
 
     &:hover {
         transform: translateY(-0.625rem) scale(1.04);
         box-shadow: 0px 0.4rem 1.5rem rgba(0, 0, 0, 0.3);
     }
+
+    &:active {
+        background-color: ${({ theme }) => theme.color.point.lightGreen};
+    }
 `;
 
-export const Thumnail = styled.img`
-    width: 100%;
+export const Thumnail = styled(BaseThumnail)`
     height: 13.125rem;
-    object-fit: cover;
-    object-position: center;
-
-    flex-shrink: 0;
-
     border-top-left-radius: ${cardBorderRadius};
     border-top-right-radius: ${cardBorderRadius};
 `;
 
-export const TextArea = styled.div`
-    display: flex;
-    padding: 0.125rem 1.25rem 0px 1.25rem;
-    flex-direction: column;
+export const TextArea = styled(BaseTextArea)`
+    padding: 0.125rem 1.25rem;
+    gap: 0.938rem;
     justify-content: center;
     align-items: flex-start;
-    gap: 0.938rem;
-    align-self: stretch;
-    overflow: hidden;
-    user-select: none;
 `;
 
-export const Title = styled.div`
-    width: 100%;
-    font-family: "SUITSemiBold";
+export const Title = styled(BaseTitle)`
     font-size: ${({ theme }) => theme.fontSize.h4};
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
 `;
 
-export const Text = styled.div`
+export const Text = styled(BaseText)`
     height: 4.125rem;
-    font-family: "SUITRegular";
     line-height: 1.313rem;
     font-size: ${({ theme }) => theme.fontSize.text};
-    overflow: hidden;
 `;
--- frontend/src/components/Card/PostCard/PostCard.styled.js
+++ frontend/src/components/Card/PostCard/PostCard.styled.js
@@ -1,48 +1,34 @@
 import styled from "styled-components";
+import {
+    cardBorderRadius,
+    BaseCard,
+    BaseThumnail,
+    BaseTitle,
+    BaseText,
+    BaseTextArea,
+} from "../Card.styled";
 
-const cardBorderRadius = "10px";
-
-export const PostCard = styled.section`
-    display: flex;
-    flex-direction: column;
-    gap: 22px;
+export const PostCard = styled(BaseCard)`
     width: 322px;
-    box-sizing: border-box;
-    padding-bottom: 28px;
 `;
 
-export const Thumnail = styled.img`
-    width: 100%;
+export const Thumnail = styled(BaseThumnail)`
     height: 13.125rem;
-    object-fit: cover;
-    object-position: center;
-
-    flex-shrink: 0;
-
     border-radius: ${cardBorderRadius};
 `;
 
-export const TextArea = styled.div`
-    display: flex;
-    flex-direction: column;
+export const TextArea = styled(BaseTextArea)`
     gap: 14px;
     padding-left: 12px;
     padding-right: 12px;
 `;
 
-export const Title = styled.div`
-    width: 100%;
-    font-family: "SUITSemiBold";
+export const Title = styled(BaseTitle)`
     font-size: ${({ theme }) => theme.fontSize.h4};
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
 `;
 
-export const Text = styled.div`
+export const Text = styled(BaseText)`
     height: 63px;
-    font-family: "SUITRegular";
     line-height: 21px;
     font-size: ${({ theme }) => theme.fontSize.text};
-    overflow: hidden;
 `;
