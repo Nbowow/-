f37e0e6a18ad0f85e0b6dbde50078075c66a1d69
신재건 <guneleven@gmail.com>
2024-09-23 16:43:30 +0900

    Update Recipe.jsx

--- frontend/src/pages/Recipe.jsx
+++ frontend/src/pages/Recipe.jsx
@@ -1,10 +1,6 @@
 import Category from "../components/Category/Category";
 import CategoryIcon from "../components/Category/CategoryIcon";
 
-// This is the Recipe page
-// It contains the CategoryIcon and Category components
-// The CategoryIcon component displays the icons for different categories
-// The Category component displays the recipes for each category
 const Recipe = () => {
     return (
         <div>
