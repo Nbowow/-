88c76b30dd506030575ecb680240b1358cf91e85
unknown <ks00919@naver.com>
2024-09-25 10:20:21 +0900

    design: #S11P21C206-261 헤더 디자인 변경

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,12 +6,12 @@ const Header = () => {
     return (
         <S.MenuBar>
             <img src={imagePath} />
-            <S.List>
-                <S.ListItem>모든 레시피</S.ListItem>
-                <S.ListItem>물가 동향</S.ListItem>
-                <S.ListItem>레시피 등록</S.ListItem>
-                <S.ListItem>마이 페이지</S.ListItem>
-            </S.List>
+            <S.LinkContainer>
+                <S.Link>모든 레시피</S.Link>
+                <S.Link>물가 동향</S.Link>
+                <S.Link>레시피 등록</S.Link>
+                <S.Link>마이 페이지</S.Link>
+            </S.LinkContainer>
         </S.MenuBar>
     );
 };
--- frontend/src/components/Header/Header.styled.js
+++ frontend/src/components/Header/Header.styled.js
@@ -13,15 +13,42 @@ export const LogoImage = styled.img`
     height: 3.125rem;
 `;
 
-export const List = styled.ul`
-    list-style-type: none;
+export const LinkContainer = styled.ul`
+    display: flex;
+    align-items: center;
+    gap: 1.4rem;
+    list-style: none;
 `;
 
-export const ListItem = styled.li`
+export const Link = styled.li`
     font-family: "SuitSemiBold";
-    font-size: ${({ theme }) => theme.fontSize.h3};
-
+    font-size: ${({ theme }) => theme.fontSize.h4};
+    list-style: none;
     cursor: pointer;
+    user-select: none;
+
+    padding: 0.5rem;
+    position: relative;
     display: inline-block;
-    margin-right: 2rem;
+
+    &::before {
+        content: "";
+        position: absolute;
+        width: 100%;
+        height: 3px;
+        background-color: ${({ theme }) => theme.color.point.green};
+        border-radius: 5px;
+        bottom: -10px;
+        left: 0;
+        transform: scaleX(0);
+        transition: all 0.6s ease;
+    }
+
+    &:hover::before {
+        transform: scaleX(1);
+    }
+
+    &:active {
+        color: ${({ theme }) => theme.color.point.green};
+    }
 `;
