212ba863c3fef4f89b73892a5127ef25f5edec87
정민 <jmeve24@naver.com>
2024-09-26 17:53:22 +0900

    design: #S11P21C206-257 토글그룹 추가 및  CSS 수정

--- frontend/src/components/RecipeDetail/RecipeHeader/RecipeHeader.styled.js
+++ frontend/src/components/RecipeDetail/RecipeHeader/RecipeHeader.styled.js
@@ -2,9 +2,8 @@ import styled from "styled-components";
 import { flexAlignEndStyle, flexBetweenStyle } from "../../../styles/common";
 
 export const RecipeHeaderContainer = styled.div`
-    width: 80%;
-    height: 10rem;
-
+    width: 90%;
+    margin: 1rem;
     padding: 1rem;
 `;
 
@@ -17,9 +16,8 @@ export const TopSection = styled.div`
 export const Title = styled.div`
     ${flexAlignEndStyle}
     height: 100%;
-
-    font-size: ${({ theme }) => theme.fontSize.h1};
-    font-family: ${({ theme }) => theme.fontWeight.bold};
+    font-size: ${({ theme }) => theme.fontSize.h2};
+    font-family: ${({ theme }) => theme.fontWeight.semiBold};
 `;
 
 export const TagWrapper = styled.div`
@@ -36,8 +34,8 @@ export const RecipeStats = styled.div`
 `;
 
 export const RecipeDescription = styled.div`
-    font-size: ${({ theme }) => theme.fontSize.h4};
-    font-family: ${({ theme }) => theme.fontWeight.semiBold};
+    font-size: ${({ theme }) => theme.fontSize.text};
+    font-family: ${({ theme }) => theme.fontWeight.medium};
     color: ${({ theme }) => theme.color.gray.darkest};
     margin-top: 1rem;
 `;
