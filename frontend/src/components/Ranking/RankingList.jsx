d4caf81158772bdc337cbd4f0a1abc4b63683cbb
신재건 <guneleven@gmail.com>
2024-09-26 10:29:02 +0900

    feat: S11P21C206-269 주석삭제

--- frontend/src/components/Ranking/RankingList.jsx
+++ frontend/src/components/Ranking/RankingList.jsx
@@ -1,7 +1,5 @@
-/* eslint-disable react/prop-types */
 import { useState, useEffect, useMemo } from "react";
 import { useTable, useSortBy } from "react-table";
-// import PropTypes from 'prop-types';
 import styled from "styled-components";
 import Pagination from "../Pagination";
 
