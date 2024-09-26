4219d5ebc3b52f0d3b7fc3375682fc8c58983032
정민 <jmeve24@naver.com>
2024-09-26 17:34:57 +0900

    design: #S11P21C206-257  댓글 컴포넌트 디자인 수정

--- frontend/src/components/Comment/Comment.jsx
+++ frontend/src/components/Comment/Comment.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import * as S from "./Comment.styled";
+import UserProfileImage from "../UserProfile/UserProfileImage/UserProfileImage";
 function Comment({ setShowReplies, comment, isReply }) {
     const toggleReplies = () => {
         setShowReplies((prev) => !prev);
@@ -9,12 +10,6 @@ function Comment({ setShowReplies, comment, isReply }) {
         ? `${comment.reply.length}개의 답글`
         : "답글 달기";
 
-    const userImage = comment.imgUrl ? (
-        <S.UserImg src={comment.imgUrl} alt={comment.user} />
-    ) : (
-        <S.UserIcon />
-    );
-
     const replyButton = !isReply ? (
         <S.ReplyButton onClick={toggleReplies}>{status}</S.ReplyButton>
     ) : null;
@@ -22,7 +17,7 @@ function Comment({ setShowReplies, comment, isReply }) {
     return (
         <S.CommentWrapper>
             <S.User>
-                {userImage}
+                <UserProfileImage imageUrl={comment.imgUrl} size={"2rem"} />
                 <S.UserName>{comment.user}</S.UserName>
             </S.User>
             <S.Text>{comment.text}</S.Text>
