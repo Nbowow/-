c212071665a6eea472cfb2835267f4e0bf0ee679
unknown <ks00919@naver.com>
2024-09-27 12:02:22 +0900

    feat: #S11P21C206-263 헤더 마이페이지 nav 추가

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
                 <S.Link>모든 레시피</S.Link>
                 <S.Link>물가 동향</S.Link>
                 <S.Link>레시피 등록</S.Link>
-                <S.Link>마이 페이지</S.Link>
+                <S.Link onClick={() => navigate("/mypage")}>마이 페이지</S.Link>
             </S.LinkContainer>
         </S.MenuBar>
     );
