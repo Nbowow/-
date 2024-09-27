c36c3f2428d083e3e6a3a523130cf88f2310add5
unknown <ks00919@naver.com>
2024-09-27 17:48:24 +0900

    feat: #S11P21C206-263 wip - 회원 프로필 리팩토링 중...

--- frontend/src/components/UserProfile/UserProfile.jsx
+++ frontend/src/components/UserProfile/UserProfile.jsx
@@ -1,32 +1,35 @@
 import * as S from "./UserProfile.styled";
 import UserProfileImage from "./UserProfileImage/UserProfileImage";
+import Button from "../Button/Button";
 import PropTypes from "prop-types";
-import CookingLevel from "./UserProfileLevel/UserProfileLevel";
 
-const UserProfile = ({
-    userId,
-    profileImgUrl,
-    backgroundImgUrl,
-    UserProfileStat,
-}) => {
+const UserProfile = ({ userId, profileImgUrl, UserProfileStat }) => {
+    const buttonText = "정보 수정";
+
     return (
-        <div>
-            <S.BackgroundImage $backgroundImgUrl={backgroundImgUrl} />
-            <S.ProfileContainer>
-                <S.ProfileImage>
-                    <UserProfileImage imageUrl={profileImgUrl} size="240px" />
-                </S.ProfileImage>
-                {UserProfileStat}
-            </S.ProfileContainer>
-            <CookingLevel />
-        </div>
+        <S.UserProfile>
+            <S.ProfileImage>
+                <UserProfileImage imageUrl={profileImgUrl} size="180px" />
+            </S.ProfileImage>
+            <S.UserStat>
+                <S.TextWrapper>
+                    <div className="nickname">흑종원</div>
+                    <div className="discription">조보아씨 일로 내려와봐유</div>
+                </S.TextWrapper>
+                <Button
+                    text={buttonText}
+                    width="80px"
+                    height="32px"
+                    type="small"
+                />
+            </S.UserStat>
+        </S.UserProfile>
     );
 };
 
 UserProfile.propTypes = {
     userId: PropTypes.string,
     profileImgUrl: PropTypes.string,
-    backgroundImgUrl: PropTypes.string,
     UserProfileStat: PropTypes.node,
 };
 
--- frontend/src/components/UserProfile/UserProfile.styled.js
+++ frontend/src/components/UserProfile/UserProfile.styled.js
@@ -1,29 +1,29 @@
 import styled from "styled-components";
 
 export const UserProfile = styled.div`
-    width: 100%;
-    height: fit-content;
-    margin: 0 auto;
-    border-radius: 0.75rem;
-    overflow: hidden;
-`;
-
-export const BackgroundImage = styled.div`
-    width: 100%;
-    height: 18.05rem;
-    background-image: url(${({ $backgroundImgUrl }) => $backgroundImgUrl});
-    background-size: cover;
-    background-position: center;
-    box-shadow: 0px 0.25rem 1.25rem rgba(0, 0, 0, 0.1);
-`;
-
-export const ProfileContainer = styled.div`
     display: flex;
-    align-items: center;
-    margin-top: -11.25rem;
-    padding: 7.5rem;
+    justify-content: space-between;
+    margin: 0 auto;
+    width: 50%;
 `;
 
-export const ProfileImage = styled.div`
-    margin: 0 auto;
+export const ProfileImage = styled.div``;
+
+export const UserStat = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+    width: 40%;
+`;
+
+export const TextWrapper = styled.div`
+    .nickname {
+        font-family: "SUITSemiBold";
+        font-size: ${({ theme }) => theme.fontSize.h3};
+    }
+
+    .discription {
+        font-family: "SUITRegular";
+        font-size: ${({ theme }) => theme.fontSize.text};
+    }
 `;
