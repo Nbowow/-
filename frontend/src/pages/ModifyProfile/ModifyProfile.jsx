94fe3bf6009696532615a2ed42d93c1a8c5bcf07
unknown <ks00919@naver.com>
2024-09-30 13:46:48 +0900

    feat: #S11P21C206-282 회원 프로필 수정 페이지 개발

--- frontend/src/pages/ModifyProfile/ModifyProfile.jsx
+++ frontend/src/pages/ModifyProfile/ModifyProfile.jsx
@@ -1,23 +1,27 @@
 import Header from "../../components/Header/Header";
 import Tab from "../../components/Tab/Tab";
 import UserProfile from "../../components/UserProfile/UserProfile";
-import CardToggleList from "../../components/CardList/CardToggleList";
+import ModifyUserInfoForm from "../../components/ModifyProfile/ModifyUserInfoForm/ModifyUserInfoForm";
+import AllergyListForm from "../../components/ModifyProfile/AllergyListForm/AllergyListForm";
+import Footer from "../../components/Footer/Footer";
+import * as S from "./ModifyProfile.styled";
 
 const ModifyProfile = () => {
     const tabs = [
-        { label: "나의 정보 수정", content: <div>나의 정보 수정</div> },
+        { label: "나의 정보 수정", content: <ModifyUserInfoForm /> },
         {
             label: "나의 알러지 정보 수정",
-            content: <CardToggleList />,
+            content: <AllergyListForm />,
         },
     ];
 
     return (
-        <div>
+        <S.ModifyProfile>
             <Header />
             <UserProfile />
             <Tab tabs={tabs} />
-        </div>
+            <Footer />
+        </S.ModifyProfile>
     );
 };
 
