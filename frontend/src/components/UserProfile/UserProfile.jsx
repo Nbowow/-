207c5cd76a28b791f4ff450ba4259e233232b849
unknown <ks00919@naver.com>
2024-09-30 09:35:15 +0900

    feat: #S11P21C206-282 유저프로필 편집

--- frontend/src/components/UserProfile/UserProfile.jsx
+++ frontend/src/components/UserProfile/UserProfile.jsx
@@ -4,7 +4,13 @@ import Button from "../Button/Button";
 import PropTypes from "prop-types";
 import { formatNumber } from "../../util/format-number";
 
-const UserProfile = ({ userId, profileImgUrl, nickName, discription }) => {
+const UserProfile = ({
+    userId,
+    showInfo,
+    profileImgUrl,
+    nickName,
+    discription,
+}) => {
     const buttonText = "정보 수정";
     const follow = formatNumber(0);
     const following = formatNumber(2000000);
@@ -14,34 +20,37 @@ const UserProfile = ({ userId, profileImgUrl, nickName, discription }) => {
             <S.ProfileImage>
                 <UserProfileImage imageUrl={profileImgUrl} size="180px" />
             </S.ProfileImage>
-            <S.UserStat>
-                <S.TextWrapper>
-                    <div className="nickname">{nickName}</div>
-                    <div className="discription">{discription}</div>
-                    <S.StatWrapper>
-                        <S.Stat>
-                            <div className="stat">팔로우</div>{" "}
-                            <div>{follow}</div>
-                        </S.Stat>
-                        <S.Stat>
-                            <div className="stat">팔로잉</div>{" "}
-                            <div>{following}</div>
-                        </S.Stat>
-                    </S.StatWrapper>
-                </S.TextWrapper>
-                <Button
-                    text={buttonText}
-                    width="80px"
-                    height="32px"
-                    type="small"
-                />
-            </S.UserStat>
+            {showInfo && (
+                <S.UserStat>
+                    <S.TextWrapper>
+                        <div className="nickname">{nickName}</div>
+                        <div className="discription">{discription}</div>
+                        <S.StatWrapper>
+                            <S.Stat>
+                                <div className="stat">팔로우</div>{" "}
+                                <div>{follow}</div>
+                            </S.Stat>
+                            <S.Stat>
+                                <div className="stat">팔로잉</div>{" "}
+                                <div>{following}</div>
+                            </S.Stat>
+                        </S.StatWrapper>
+                    </S.TextWrapper>
+                    <Button
+                        text={buttonText}
+                        width="80px"
+                        height="32px"
+                        type="small"
+                    />
+                </S.UserStat>
+            )}
         </S.UserProfile>
     );
 };
 
 UserProfile.propTypes = {
     userId: PropTypes.string,
+    showInfo: PropTypes.bool,
     profileImgUrl: PropTypes.string,
     UserProfileStat: PropTypes.node,
     nickName: PropTypes.string,
