a5fc68f5e15a449c3d3a6f91afb09cbc8413e93c
정민 <jmeve24@naver.com>
2024-10-01 13:30:13 +0900

    feat: #S11P21C206-285 태그 컴포넌트 클릭 함수 추가
    - 월간/주간/일간 클릭 시 변경 확인하기 위해 추가

--- frontend/src/components/Tag/Tag.styled.js
+++ frontend/src/components/Tag/Tag.styled.js
@@ -4,20 +4,25 @@ import { flexCenterStyle } from "../../styles/common";
 export const TagEmoji = styled.span`
     font-size: ${({ theme }) => theme.fontSize.text};
     font-family: "TossFace";
+    margin-left: 0.5rem;
 `;
 export const TagName = styled.span`
-    margin-right: 0.5rem;
     font-family: ${({ theme }) => theme.fontWeight.bold};
-    color: ${({ theme }) => theme.color.point.lightGreen};
+    color: ${({ isClicked, theme }) =>
+        isClicked ? theme.color.point.lightGreen : theme.color.point.green};
 `;
 export const TagWrapper = styled.div`
     ${flexCenterStyle}
     font-size: ${({ theme }) => theme.fontSize.text};
-    background-color: ${({ theme }) => theme.color.point.green};
+    background-color: ${({ isClicked, theme }) =>
+        isClicked && theme.color.point.green};
+
+    border: 1px solid
+        ${({ isClicked, theme }) =>
+            isClicked ? theme.color.point.lightGreen : theme.color.point.green};
     border-radius: ${({ theme }) => theme.borderRadius.large};
     text-align: center;
-
     padding: 0.3rem 0.6rem;
-
+    cursor: pointer;
     inline-size: max-content;
 `;
