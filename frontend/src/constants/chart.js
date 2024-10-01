a8adbba549c43822298ecb26a714ce6a12ecd03f
정민 <jmeve24@naver.com>
2024-10-01 13:28:12 +0900

    feat: #S11P21C206-285 차트 관련 설정 추가
    - 기본 차트 컴포넌트에 config props 추가하는걸로 변경
    - pieChart 추가
    - 작은 라인 차트  컴포넌트 추가

--- frontend/src/constants/chart.js
+++ frontend/src/constants/chart.js
@@ -4,3 +4,16 @@ export const monthLabels = () =>
 export const barLabels = () => {
     return ["5", "4", "3", "2", "1"];
 };
+
+export const pieLabels = () => {
+    return [
+        "단백질",
+        "탄수화물",
+        "지방",
+        "나트륨",
+        "당류",
+        "콜레스테롤",
+        "포화지방산",
+        "트랜스지방산",
+    ];
+};
