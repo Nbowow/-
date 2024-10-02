b35a3bf259b23a7a721fd770441a40753a15c336
unknown <ks00919@naver.com>
2024-10-02 15:27:47 +0900

    chore: #S11P21C206-283 react query 설치

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import GlobalStyle from "./styles/global.js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 1,
+            staleTime: 5 * 60 * 1000,
+            cacheTime: 10 * 60 * 1000,
+            refetchOnWindowFocus: false,
+        },
+    },
+});
 
 createRoot(document.getElementById("root")).render(
     <>
-        <GlobalStyle />
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <GlobalStyle />
+            <App />
+        </QueryClientProvider>
     </>,
 );
