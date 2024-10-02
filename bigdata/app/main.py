a58a101eb625737b93f761b68587411076318ba5
hkk3626 <hkk3626@naver.com>
2024-10-02 15:09:11 +0900

    feat: #S11P21C206-203 db table 생성코드 추가

--- bigdata/app/main.py
+++ bigdata/app/main.py
@@ -8,14 +8,14 @@ from app.database.base import Base
 from app.database.database import engineconn
 
 # 모델을 명시적으로 임포트 (필수)
+from app.models import Users, DayPrice, Materials, MonthPrice, Nutrients, RecipeMaterials, RecipeNutrient, RecipeOrders, \
+    Recipes, WeekPrice, YearPrice  # noqa
 
 app = FastAPI()
 
-app.include_router(RecipeController.router)
-app.include_router(PriceController.router)
-app.include_router(CrawlingController.router)
 
-if __name__ == "__main__":
+@app.on_event("startup")
+async def startup_event():
     engine = engineconn()
     try:
         connection = engine.connection()  # 데이터베이스 연결 확인
@@ -23,11 +23,15 @@ if __name__ == "__main__":
     except Exception as e:
         print(f"데이터베이스 연결 실패: {e}")
 
-    session = engine.sessionmaker()
-
     print("등록된 테이블 목록:", Base.metadata.tables)
 
     # 테이블 자동 생성
     Base.metadata.create_all(engine.engine)
 
+
+app.include_router(RecipeController.router)
+app.include_router(PriceController.router)
+app.include_router(CrawlingController.router)
+
+if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
