4ae5ce68e6eea6b7d7cfeccd72f1b05fbc668e85
gichangee <parkgc0504@naver.com>
2024-10-03 18:29:16 +0900

    fix : 소셜 에러 해결

--- social_service/src/main/java/com/recipe/social_service/repository/SocialRepository.java
+++ social_service/src/main/java/com/recipe/social_service/repository/SocialRepository.java
@@ -2,15 +2,19 @@ package com.recipe.social_service.repository;
 
 import com.recipe.social_service.data.domain.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 
 public interface SocialRepository extends JpaRepository<Follow, Long> {
 
-    // 팔로워 목록 조회
-    List<Long> findByFollowingId(Long userId);
+    // 팔로워 ID 목록 조회
+    @Query("SELECT f.followerId FROM Follow f WHERE f.followingId = :userId")
+    List<Long> findFollowerIdsByFollowingId(@Param("userId") Long userId);
 
-    // 팔로잉 목록 조회
-    List<Long> findByFollowerId(Long userId);
+    // 팔로잉 ID 목록 조회
+    @Query("SELECT f.followingId FROM Follow f WHERE f.followerId = :userId")
+    List<Long> findFollowingIdsByFollowerId(@Param("userId") Long userId);
 }
