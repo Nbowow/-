0f57b65ee1bb74b0760b3837906da53178982128
gichangee <parkgc0504@naver.com>
2024-10-03 18:31:29 +0900

    fix : 소셜 에러 해결

--- social_service/src/main/java/com/recipe/social_service/service/SocialService.java
+++ social_service/src/main/java/com/recipe/social_service/service/SocialService.java
@@ -17,12 +17,12 @@ public class SocialService {
     // 팔로워 목록 조회
     public List<Long> getFollowers(Long userId) {
         // 사용자가 팔로우하는 다른 사람들의 ID 목록을 가져옴
-        return socialRepository.findByFollowingId(userId);
+        return socialRepository.findFollowerIdsByFollowingId(userId);
     }
 
     // 팔로잉 목록 조회
     public List<Long> getFollowings(Long userId) {
         // 사용자를 팔로우하는 사람들의 ID 목록을 가져옴
-        return socialRepository.findByFollowerId(userId);
+        return socialRepository.findFollowingIdsByFollowerId(userId);
     }
 }
