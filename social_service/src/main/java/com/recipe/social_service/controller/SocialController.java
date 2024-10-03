5569245dcdad28e200bff1364d4d788e1ff514e6
gichangee <parkgc0504@naver.com>
2024-10-03 21:13:14 +0900

    feat : 팔로우 추가 및 삭제 기능

--- social_service/src/main/java/com/recipe/social_service/controller/SocialController.java
+++ social_service/src/main/java/com/recipe/social_service/controller/SocialController.java
@@ -1,5 +1,6 @@
 package com.recipe.social_service.controller;
 
+import com.recipe.social_service.client.UserServiceClient;
 import com.recipe.social_service.service.SocialService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,19 +14,33 @@ import java.util.List;
 @RequestMapping("/api/v1/social")
 public class SocialController {
 
+    private final UserServiceClient userServiceClient;
     private final SocialService socialService;
 
-    // 팔로워 목록 조회
     @GetMapping("/{userId}/follower")
-    public ResponseEntity<List<Long>> getFollowers(@PathVariable Long userId) {
+    public ResponseEntity<List<Long>> getFollowers(@PathVariable("userId") Long userId) {
         List<Long> followers = socialService.getFollowers(userId);
         return ResponseEntity.status(HttpStatus.OK).body(followers);
     }
 
-    // 팔로잉 목록 조회
     @GetMapping("/{userId}/following")
-    public ResponseEntity<List<Long>> getFollowings(@PathVariable Long userId) {
+    public ResponseEntity<List<Long>> getFollowings(@PathVariable("userId") Long userId) {
         List<Long> followings = socialService.getFollowings(userId);
         return ResponseEntity.status(HttpStatus.OK).body(followings);
+    }
+
+
+    @PostMapping("/follow/{partnerId}")
+    public ResponseEntity<?> follow(@RequestHeader("Authorization") String authorization, @PathVariable("partnerId") Long partnerId) {
+        Long userId = userServiceClient.getUserId(authorization);
+        socialService.followUser(userId, partnerId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/unfollow/{partnerId}")
+    public ResponseEntity<?> unfollow(@RequestHeader("Authorization") String authorization, @PathVariable("partnerId") Long partnerId) {
+        Long userId = userServiceClient.getUserId(authorization);
+        socialService.unfollowUser(userId, partnerId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
