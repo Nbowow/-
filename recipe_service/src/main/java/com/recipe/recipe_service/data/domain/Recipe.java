06776452dd8d8e06d57c6dfc86e00cb2d8e1e813
nbo5548 <nbo5548@naver.com>
2024-10-03 14:09:15 +0900

    feat: recipe domain

--- recipe_service/src/main/java/com/recipe/recipe_service/data/domain/Recipe.java
+++ recipe_service/src/main/java/com/recipe/recipe_service/data/domain/Recipe.java
@@ -1,23 +1,23 @@
 package com.recipe.recipe_service.data.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.ws.rs.DefaultValue;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "recipes")
+@Table(name = "Recipes")
 public class Recipe {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long recipeId;
-    private Long userId;
+    @Column(name = "recipe_id")
+    private Long id;
 
     @Column(name = "recipe_title")
     private String title;
@@ -25,8 +25,41 @@ public class Recipe {
     @Column(name = "recipe_name")
     private String name;
 
-    @Column(name = "recipe_info")
-    private String info;
+    @Column(name = "recipe_intro")
+    private String intro; // 레시피 소개
+
+    @Column(name = "recipe_image")
+    private String image;
+
+    @Column(name = "recipe_like_count")
+    private Long likeCount;
+
+    @Column(name = "recipe_view_count")
+    private Long viewCount;
+
+    @Column(name = "recipe_servings")
+    private Long servings;
+
+    @Column(name = "recipe_time")
+    private Long time;
+
+    @Column(name = "recipe_level")
+    private String level;
+
+    private String type;
+
+    private String situation;
+
+    private String ingredients;
+
+    private String method;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime modifiedDate;
+
+    @DefaultValue(value = "true")
+    private Boolean userStatus;
 
 
 }
