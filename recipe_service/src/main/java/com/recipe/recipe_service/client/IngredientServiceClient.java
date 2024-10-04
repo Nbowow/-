3edf312a22b665d7e6d8023623ee06cbc53efbbf
nbo5548 <nbo5548@naver.com>
2024-10-04 15:58:27 +0900

    fix: ingredient Id 받는 api 경로 수정

--- recipe_service/src/main/java/com/recipe/recipe_service/client/IngredientServiceClient.java
+++ recipe_service/src/main/java/com/recipe/recipe_service/client/IngredientServiceClient.java
@@ -2,11 +2,12 @@ package com.recipe.recipe_service.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "ingredient-service")
 public interface IngredientServiceClient {
 
-    @GetMapping("/api/v1/ingredient/search")
-    Long getIngredientIdByName(@RequestParam("name") String name);
+    @GetMapping("/api/v1/ingredient/get-num/{name}")
+    Long getIngredientIdByName(@PathVariable("name") String name);
 }
