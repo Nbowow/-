b0b91fb2944e1be5ffc0da5e42c8503d5eca5a34
이지영 <hkk3626@naver.com>
2024-10-06 12:11:36 +0900

    relase: 재료 API 작성

---
+++ ingredient_service/src/main/java/com/recipe/ingredient_service/data/dto/recipe/response/RecipeResponseDto.java
@@ -0,0 +1,28 @@
+package com.recipe.ingredient_service.data.dto.recipe.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RecipeResponseDto {
+    private Long id;
+    private String title;
+    private String name;
+    private String info;
+    private String image;
+    private Long viewCount;
+    private Long servings;
+    private Long time;
+    private String level;
+    private String cookingTools;
+    private String type;
+    private String situation;
+    private String ingredients;
+    private String method;
+    private Long userId;
+}
--- ingredient_service/src/main/java/com/recipe/ingredient_service/repository/IngredientRepository.java
+++ ingredient_service/src/main/java/com/recipe/ingredient_service/repository/IngredientRepository.java
@@ -1,13 +1,27 @@
 package com.recipe.ingredient_service.repository;
 
 import com.recipe.ingredient_service.data.domain.Ingredient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
 
     Optional<Ingredient> findByNameIgnoreCase(String name);
+
+    Page<Ingredient> findAll(Pageable pageable);
+
+    @Query("SELECT i.id FROM Ingredient i WHERE i.priceStatus = true")
+    List<Long> findMaterialIdsWithPriceStatusTrue();
+
+    @Query("SELECT i FROM Ingredient i WHERE i.id IN :ingredientIds")
+    List<Ingredient> findByIds(@Param("ingredientIds") List<Long> ingredientIds);
+
 }
