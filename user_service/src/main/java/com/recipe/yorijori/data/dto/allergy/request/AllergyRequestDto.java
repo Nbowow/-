9b5f16ce9b47a1ba0a59a5bc7d197a163d8be428
gichangee <parkgc0504@naver.com>
2024-10-06 18:04:17 +0900

    feat : 알러지 요청 생성자 추가

--- user_service/src/main/java/com/recipe/yorijori/data/dto/allergy/request/AllergyRequestDto.java
+++ user_service/src/main/java/com/recipe/yorijori/data/dto/allergy/request/AllergyRequestDto.java
@@ -2,9 +2,11 @@ package com.recipe.yorijori.data.dto.allergy.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class AllergyRequestDto {
     private String commonCodeNum;  // 공통코드 번호
 }
