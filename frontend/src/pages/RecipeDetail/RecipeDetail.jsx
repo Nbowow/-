f6592d0609a059c4832c329d16f6729327f440ca
정민 <jmeve24@naver.com>
2024-10-07 17:10:48 +0900

    feat: #S11P21C206-295  레시피 API  연결

--- frontend/src/pages/RecipeDetail/RecipeDetail.jsx
+++ frontend/src/pages/RecipeDetail/RecipeDetail.jsx
@@ -1,26 +1,38 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import RecipeUser from "../../components/RecipeDetail/RecipeUser/RecipeUser";
 import RecipeHeader from "../../components/RecipeDetail/RecipeHeader/RecipeHeader";
 import RecipeStep from "../../components/RecipeStep/RecipeStep";
-import { useEffect } from "react";
 import RecipeInfo from "../../components/RecipeDetail/RecipeInfo";
 import Comments from "../../components/Comment/Comments";
 import Review from "../../components/Review/Review";
 import * as S from "./RecipeDetail.styled";
+import { getRecipe, getReviews, getComments } from "../../api/recipe";
+
 const RecipeDetail = () => {
+    const [recipe, setRecipes] = useState({});
+    const [reviews, setReviews] = useState([]);
+    const [comments, setComments] = useState([]);
     const { id } = useParams();
 
-    useEffect(() => {}, [id]);
-
-    const reviews = [
-        {
-            id: 1,
-            rating: 5,
-            title: "맛있어용",
-            content: "굿굿굿굿",
-            imgUrl: "https://cdn.pixabay.com/photo/2021/07/19/16/04/pizza-6478478_1280.jpg",
-        },
-    ];
+    useEffect(() => {
+        const fetchRecipe = async () => {
+            const data = await getRecipe(id);
+            setRecipes(data);
+        };
+        const fetchReview = async () => {
+            const data = await getReviews(id);
+            setReviews(data);
+        };
+        const fetchComment = async () => {
+            const data = await getComments(id);
+            setComments(data);
+        };
+        fetchRecipe(id);
+        fetchReview(id);
+        fetchComment(id);
+    }, [id]);
 
     const allergies = ["토마토", "우유"];
 
@@ -32,46 +44,6 @@ const RecipeDetail = () => {
         talk: " 명수옹의 요리조리요리조리요리조리요리조리",
     };
 
-    const data = {
-        tag: ["초스피드", "간식"],
-        title: "명수의 초간단피자 만들기",
-        text: "맛있는 피자를 만들어 봅시다!",
-    };
-
-    const data2 = {
-        recipe: {
-            minute: 30,
-            difficulty: "초급",
-            calories: 500,
-            cost: 5000,
-        },
-        ingredients: [
-            { name: "밀가루", amount: "200g" },
-            { name: "토마토소스", amount: "100g" },
-            { name: "치즈", amount: "100g" },
-            { name: "피자토핑", amount: "100g" },
-            { name: "버섯", amount: "50g" },
-            { name: "올리브", amount: "30g" },
-            { name: "파프리카", amount: "70g" },
-        ],
-    };
-
-    const comment = [
-        {
-            imgUrl: "https://cdn.pixabay.com/photo/2021/07/19/16/04/pizza-6478478_1280.jpg",
-            user: "명수옹",
-            text: "맛있어용",
-            date: "2021-07-19",
-            reply: [
-                {
-                    imgUrl: "https://cdn.pixabay.com/photo/2021/07/19/16/04/pizza-6478478_1280.jpg",
-                    user: "명수옹",
-                    text: "맛있어용",
-                    date: "2021-07-19",
-                },
-            ],
-        },
-    ];
     const step = [
         {
             img: "https://cdn.pixabay.com/photo/2021/07/19/16/04/pizza-6478478_1280.jpg",
@@ -88,26 +60,25 @@ const RecipeDetail = () => {
         <>
             <S.Container>
                 <S.InfoSection>
-                    <RecipeHeader data={data} />
-                    <RecipeInfo data={data2} allergies={allergies} />
+                    <RecipeHeader recipe={recipe} />
+                    {/* todo: RecipeInfo 컴포넌트에 allergies props전달 */}
+                    <RecipeInfo recipe={recipe} allergies={allergies} />
                 </S.InfoSection>
 
                 <S.StepSection>
+                    {/* todo: RecipeStep 컴포넌트에 step props 전달 */}
                     <RecipeStep recipe={step} />
                 </S.StepSection>
 
                 <S.UserSection>
-                    <RecipeUser data={user} />
+                    {/* todo : recipeUser 컴포넌트에 user props 전달 */}
+                    <RecipeUser user={user} />
                 </S.UserSection>
 
-                <Review
-                    reviews={reviews}
-                    rating={rating}
-                    recipe={data2.recipe}
-                />
+                <Review reviews={reviews} rating={rating} recipe={recipe} />
                 <S.CommentSection>
-                    <S.CommentTitle>댓글 {comment.length}개</S.CommentTitle>
-                    <Comments comments={comment} />
+                    <S.CommentTitle>댓글 {comments.length}개</S.CommentTitle>
+                    <Comments initComments={comments} id={id} />
                 </S.CommentSection>
             </S.Container>
         </>
