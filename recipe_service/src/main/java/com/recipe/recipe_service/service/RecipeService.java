d1fafda67b422a8d61c8642fedbfa0a2f424160a
박기창 <parkgc0504@naver.com>
2024-10-08 00:43:31 +0900

    fix : 레시피 오류 해결

--- recipe_service/src/main/java/com/recipe/recipe_service/service/RecipeService.java
+++ recipe_service/src/main/java/com/recipe/recipe_service/service/RecipeService.java
@@ -13,7 +13,6 @@ import com.recipe.recipe_service.repository.*;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
