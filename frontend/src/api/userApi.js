3960cd670e6d44a1ecad599b55fc0fa02336afa9
정민 <jmeve24@naver.com>
2024-10-07 17:47:09 +0900

    feat: #S11P21C206-291 유저 재료 좋아요한 API 연결

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -4,3 +4,7 @@ export const fetchUser = async () => {
     const response = await axiosInstance.get("/users/user");
     return response.data;
 };
+export const getUserLike = async () => {
+    const response = await axiosInstance.get("/users/like");
+    return response.data;
+};
