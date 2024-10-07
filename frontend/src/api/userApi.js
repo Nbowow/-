faa9337014921f95fff03754868bc3d430129c75
Minju <69795199+ks00919@users.noreply.github.com>
2024-10-08 02:20:10 +0900

    feat: #S11P21C206-296 회원 프로필 수정 요청 함수 작성

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -38,3 +38,19 @@ export const deleteAllergy = async (commonCodeNum) => {
     );
     return response.data;
 };
+
+export const uploadProfileImage = async (file) => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const response = await axiosInstance.post(
+        "/users/update-profile",
+        formData,
+        {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        },
+    );
+    return response.data;
+};
