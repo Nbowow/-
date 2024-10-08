dcd9b5abb1ef3b7fa7607587440095a43df0550e
정민 <jmeve24@naver.com>
2024-10-08 17:06:49 +0900

    feat:  #S11P21C206-295 유저 팔로우/팔로잉 API 연결

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -4,6 +4,17 @@ export const fetchUser = async () => {
     const response = await axiosInstance.get("/users/user");
     return response.data;
 };
+
+export const followUser = async (id) => {
+    const response = await axiosInstance.post(`/social/follow/${id}`);
+    return response.data;
+};
+
+export const unFollowUser = async (id) => {
+    const response = await axiosInstance.delete(`/social/follow/${id}`);
+    return response.data;
+};
+
 export const getCommonCode = async () => {
     const response = await axiosInstance.get("/users/common");
     return response.data;
