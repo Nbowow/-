c1e033084cafb65b3ad07f01ee3351b920a7ae48
gichangee <parkgc0504@naver.com>
2024-10-09 16:48:25 +0900

    재료 오타 수정 기능 추가

--- ingredient_service/src/main/java/com/recipe/ingredient_service/repository/IngredientRepository.java
+++ ingredient_service/src/main/java/com/recipe/ingredient_service/repository/IngredientRepository.java
@@ -27,4 +27,6 @@ public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
     @Query("SELECT i FROM Ingredient i WHERE i.id IN :ingredientIds")
     List<Ingredient> findByIds(@Param("ingredientIds") List<Long> ingredientIds);
 
+    @Query("SELECT r.name FROM Ingredient r")
+    List<String> findAllIngredientNames();
 }
