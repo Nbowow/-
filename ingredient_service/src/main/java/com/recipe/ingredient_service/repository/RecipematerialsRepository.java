b5f91d5015c86e85537bdcd8e8baeb40622bff65
gichangee <parkgc0504@naver.com>
2024-10-09 13:47:34 +0900

    fix : 재료와 연관된 레시피 리턴 수정

--- ingredient_service/src/main/java/com/recipe/ingredient_service/repository/RecipematerialsRepository.java
+++ ingredient_service/src/main/java/com/recipe/ingredient_service/repository/RecipematerialsRepository.java
@@ -2,10 +2,19 @@ package com.recipe.ingredient_service.repository;
 
 
 import com.recipe.ingredient_service.data.domain.Recipematerials;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RecipematerialsRepository extends JpaRepository<Recipematerials, Long> {
-    List<Recipematerials> findByMaterialIdIn(List<Long> materialIds);
+
+    // 재료 아이디가 모두 포함된 레시피만 조회하는 JPQL 쿼리, 페이징을 추가하여 최대 3개만 반환
+    @Query("SELECT rm.recipeId FROM Recipematerials rm WHERE rm.materialId IN :ingredientIds " +
+            "GROUP BY rm.recipeId HAVING COUNT(rm.recipeId) = :ingredientCount")
+    List<Long> findRecipeIdsByAllIngredientIds(@Param("ingredientIds") List<Long> ingredientIds,
+                                               @Param("ingredientCount") Long ingredientCount,
+                                               Pageable pageable);
 }
