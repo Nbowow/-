fb0676f583aef63857cc4986b45758053f8cf279
정민 <jmeve24@naver.com>
2024-10-10 15:05:09 +0900

    feat: #S11P21C206-288 실시간 물가 선택시 검색 창으로 이동

--- frontend/src/components/IngredientPrices/LivePriceTracker/LivePriceTracker.jsx
+++ frontend/src/components/IngredientPrices/LivePriceTracker/LivePriceTracker.jsx
@@ -1,19 +1,40 @@
 import PropTypes from "prop-types";
 import * as S from "./LivePriceTracker.styled";
+import { useEffect, useState } from "react";
 
-const LivePriceTracker = ({ ingredient }) => {
+const LivePriceTracker = ({ ingredient, handleClick }) => {
+    const [isExpensive, setIsExpensive] = useState(false);
+
+    useEffect(() => {
+        if (ingredient.priceGap < 0) {
+            setIsExpensive(false);
+        } else setIsExpensive(true);
+    }, [ingredient]);
+
     return (
-        <S.Wrapper>
+        <S.Wrapper onClick={() => handleClick(ingredient.name)}>
             <S.IngredientInfo>
                 <S.IngredientInfoLayout>
                     <S.IngredientName>{ingredient.name}</S.IngredientName>
                 </S.IngredientInfoLayout>
+
+                <S.ingredientNowPrice>
+                    현재 가격 :<span> {ingredient.currentPrice}원</span>
+                </S.ingredientNowPrice>
+
                 <S.IngredientLabel>
                     어제보다
-                    <S.IngredientPrice>
-                        {ingredient.priceGap}원
+                    <S.IngredientPrice isExpensive={isExpensive}>
+                        {isExpensive
+                            ? ingredient.priceGap
+                            : -ingredient.priceGap}
+                        원
                     </S.IngredientPrice>
-                    저렴해요 !
+                    이 더
+                    <S.IngredientPrice isExpensive={isExpensive}>
+                        {isExpensive ? "상승" : "인하"}
+                    </S.IngredientPrice>
+                    했습니다.
                 </S.IngredientLabel>
             </S.IngredientInfo>
         </S.Wrapper>
@@ -22,5 +43,6 @@ const LivePriceTracker = ({ ingredient }) => {
 
 LivePriceTracker.propTypes = {
     ingredient: PropTypes.object.isRequired,
+    handleClick: PropTypes.func.isRequired,
 };
 export default LivePriceTracker;
--- frontend/src/components/IngredientPrices/YearlyPriceChart/YearlyPriceChart.jsx
+++ frontend/src/components/IngredientPrices/YearlyPriceChart/YearlyPriceChart.jsx
@@ -6,8 +6,20 @@ import { useState } from "react";
 import Chart from "../../Chart/Chart";
 
 import PropTypes from "prop-types";
+import EmptyPlaceHolder from "../../EmptyPlaceholder/EmptyPlaceHolder";
 
 const YearlyPriceChart = ({ priceHistory }) => {
+    const allZero = priceHistory.monthPrice.every((item) => item.price === 0);
+    const [viewType, setViewType] = useState(0);
+    if (allZero)
+        return (
+            <EmptyPlaceHolder
+                height="15rem"
+                width="100%"
+                content="물가 정보가 없습니다"
+                recommend={"조금만 기다려잉 ~ "}
+            />
+        );
     const priceArr = [
         priceHistory.monthPrice.map((item) => item.price),
         priceHistory.weekPrice.map((item) => item.price),
@@ -28,7 +40,6 @@ const YearlyPriceChart = ({ priceHistory }) => {
         lineOptions(yAxisMax, yAxisMin),
     );
 
-    const [viewType, setViewType] = useState(0);
     const handleClick = (status) => {
         setViewType(status);
     };
