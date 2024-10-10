a23aeca662867d61907fc2b43d274bb0de8d5057
nbo5548 <nbo5548@naver.com>
2024-10-10 11:23:30 +0900

    fix: 공통코드관련 수정

--- recipe_service/src/main/java/com/recipe/recipe_service/service/RecipeService.java
+++ recipe_service/src/main/java/com/recipe/recipe_service/service/RecipeService.java
@@ -453,6 +453,7 @@ public class RecipeService {
                             .commentCount(recipe.getCommentCount())
                             .build();
                 })
+                .limit(100)
                 .toList();
 
     }
