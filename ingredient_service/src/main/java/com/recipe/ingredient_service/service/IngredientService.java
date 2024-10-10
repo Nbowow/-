37bd0e8db22b0c56b9ef5eda51ea10bb93e354d5
이지영 <hkk3626@naver.com>
2024-10-11 03:44:29 +0900

    feat: 회원 좋아요 한 재료 조회에 price 추가

--- ingredient_service/src/main/java/com/recipe/ingredient_service/service/IngredientService.java
+++ ingredient_service/src/main/java/com/recipe/ingredient_service/service/IngredientService.java
@@ -428,7 +428,7 @@ public class IngredientService {
 
         boolean isAlreadyLiked = userLikeMaterialsRepository.existsByUserIdAndIngredientId(userId, ingredientId);
         if (isAlreadyLiked) {
-                throw new AlreadyLikedException();
+            throw new AlreadyLikedException();
         }
         UserLikeMaterials newLike = UserLikeMaterials.builder()
                 .userId(userId)
@@ -444,9 +444,23 @@ public class IngredientService {
         userLikeMaterialsRepository.deleteByUserIdAndIngredientId(userId, ingredientId);
     }
 
-    public List<Ingredient> findUserLikeIngredients(Long userId) {
+    public List<IngredientUserLikeDto> findUserLikeIngredients(Long userId) {
         List<Long> ingredientList = userLikeMaterialsRepository.findIngredientIdsByUserId(userId);
-        return ingredientRepository.findByIds(ingredientList);
+
+        return ingredientRepository.findByIds(ingredientList)
+                .stream().map(ingredient -> {
+                    Pageable pageable = PageRequest.of(0, 1);
+                    int price = dayPriceRepository.findRecentDays(ingredient.getId(), LocalDateTime.now(), pageable).get(0).getPrice();
+                    return new IngredientUserLikeDto(
+                            ingredient.getId(),
+                            ingredient.getName(),
+                            ingredient.isPriceStatus(),
+                            ingredient.getIngredientImage(),
+                            ingredient.getAllergyNum(),
+                            price
+                    );
+                })
+                .collect(Collectors.toList());
     }
 
     public Ingredient addIngredient(String name) {
